327ee6f83987d9887a15db37f3a801f985ea4759
applejxd <myamazaki.phys@gmail.com>
2021-07-04 16:24:16 +0900

    リファクタリング

--- modules/pre_process.py
+++ modules/pre_process.py
@@ -3,33 +3,40 @@ from sklearn.preprocessing import LabelEncoder
 from modules.self_logger import SelfLogger
 from util import Util
 from typing import List
-import os
 
 
 class PreProcess:
-    def __init__(self):
-        self.logger = SelfLogger.get_logger(__file__)
+    def __init__(self, file_path, target_col, cat_cols):
+        self._logger = SelfLogger.get_logger(__file__)
 
-    def categorical_encoder(self, table: pd.DataFrame, cat_cols: List[str]) -> pd.DataFrame:
+        # 読み込み
+        table = Util.read_csv(file_path)
+        self._train_x = table.drop(target_col, axis=1)
+        self._train_y = table[target_col]
+
+        # 前処理
+        self._categorical_encoder(cat_cols)
+
+    @property
+    def train_x(self):
+        return self._train_x
+
+    @property
+    def train_y(self):
+        return self._train_y
+
+    def _categorical_encoder(self, cat_cols: List[str]) -> None:
         """
         カテゴリ変数をループしてlabel encoding
         :return:
         """
-        result = table.copy()
         for category in cat_cols:
             # 学習データに基づいて定義する
             encoder = LabelEncoder()
-            encoder.fit(result[category])
-            result[category] = encoder.transform(result[category])
-        self.logger.info("Categorical variables converted.")
-        return result
-
-    def pre_process(self):
-        cat_cols = ["cut", "color", "clarity"]
-        table = Util.read_csv("../data/diamonds.csv")
-        table = self.categorical_encoder(table, cat_cols)
-        print(table)
+            encoder.fit(self._train_x[category])
+            self._train_x[category] = encoder.transform(self._train_x[category])
+        self._logger.info("Categorical variables converted.")
 
 
 if __name__ == "__main__":
-    PreProcess().pre_process()
+    PreProcess("../data/diamonds.csv", "price", ["cut", "color", "clarity"])
